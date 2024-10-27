8154b7c22832c4bc9150e6a1fe30871270946aab
tai-cha <dev@taichan.site>
2024-10-27 14:55:05 +0900

    remove unused

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,7 @@
 /* eslint-disable react/no-unused-prop-types */
 'use strict';
 import {Text} from 'ink'
-import terminalImage from 'terminal-image'
 import React, { useState, useEffect } from 'react'
-import fileType from 'file-type'
 import { convertToSixel } from './encode.js';
 
 
@@ -15,13 +13,6 @@ type Props = {
 	maximumFrameRate?: number
 }
 
-type GifProps = {
-	src: Buffer | string,
-	width?: number,
-	height?: number,
-	preserveAspectRatio?: boolean,
-	maximumFrameRate?: number
-}
 
 const Image = (props:Props):JSX.Element => {
 	const [imageData, setImageData] = useState('');
